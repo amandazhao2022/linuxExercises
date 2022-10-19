6d4b571e206e96e503ce6bc56dc067cb98e7566b
WanxinTu <wanxintu5013@gmail.com>
2022-10-19 16:38:59 -0500

    find 100..2000

--- digits.sh
+++ digits.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+sum=0
+
+for i in {1000..2000}
+do d1=$(($i%1000%100%10))
+   if [[ (($d1 -eq 1) || ($d1 -eq 0)) ]]
+   then
+       echo $i
+       d2=$(($i%1000%100/10))
+       if [[ (($d2 -eq 1) || ($d2 -eq 0)) ]]
+       then
+	   d3=$(($i%1000/100))
+	   if [[ (($d3 -eq 1) || ($d3 -eq 0)) ]]
+	   then
+	       d4=$(($i/1000))
+	       if [[ ($d4 -eq 1) ]]
+	       then
+		   sum=$(($sum+$i))
+	       fi
+
+done
+echo $sum
