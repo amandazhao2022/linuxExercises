a3524ef4f476239a52d03ecbf44cc6a9f88a8e12
WanxinTu <wanxintu5013@gmail.com>
2022-10-20 10:29:14 -0500

    digits.sh finished

--- digits.sh
+++ digits.sh
@@ -6,7 +6,6 @@ for i in {1000..2000}
 do d1=$(($i%1000%100%10))
    if [[ (($d1 -eq 1) || ($d1 -eq 0)) ]]
    then
-       echo $i
        d2=$(($i%1000%100/10))
        if [[ (($d2 -eq 1) || ($d2 -eq 0)) ]]
        then
@@ -14,10 +13,12 @@ do d1=$(($i%1000%100%10))
 	   if [[ (($d3 -eq 1) || ($d3 -eq 0)) ]]
 	   then
 	       d4=$(($i/1000))
-	       if [[ ($d4 -eq 1) ]]
-	       then
-		   sum=$(($sum+$i))
+               if [[ ($d4 -eq 1) ]]
+               then
+		   sum=$(($sum + $i))
 	       fi
-
+	   fi
+       fi
+   fi
 done
 echo $sum
